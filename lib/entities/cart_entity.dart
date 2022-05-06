13a747090947c7d7e34b98e6a036fb6b574190f8
Talis <talismarchioro@gmail.com>
2022-05-05 22:37:26 -0300

    Adicionando entidades

--- lib/entities/cart_entity.dart
+++ lib/entities/cart_entity.dart
@@ -1,3 +1,9 @@
+import 'package:uudi_challenge/entities/product_entity.dart';
+
 class CartEntity {
+  List<ProductEntity> products;
   
+  CartEntity({
+    required this.products,
+  });
 }
--- lib/entities/product_entity.dart
+++ lib/entities/product_entity.dart
@@ -1,3 +1,22 @@
 class ProductEntity {
-  
+  String name;
+  String description;
+  num price;
+  String image;
+
+  ProductEntity({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+
+  factory ProductEntity.fromMap(Map<String, dynamic> map) {
+    return ProductEntity(
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      price: map['price'] ?? 0,
+      image: map['image'] ?? '',
+    );
+  }
 }
