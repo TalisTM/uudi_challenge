11d563bf4d7705690e343b3a2d720f5d6d9e5c7c
Talis <talismarchioro@gmail.com>
2022-05-06 15:03:09 -0300

    registrando dependencias de singleton do carrinho

--- lib/app/dependencies_init.dart
+++ lib/app/dependencies_init.dart
@@ -1,5 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:uudi_challenge/stores/cart_store.dart';
+
 class Dependencies {
   static void init() {
-    
+    try {
+      GetIt getIt = GetIt.I;
+      getIt.registerSingleton<CartStore>(CartStore());
+    } catch (e) {
+      null;
+    }
   }
 }
