6e3f330146dd3f9079d984999e5d667b03cef37f
Talis <talismarchioro@gmail.com>
2022-05-06 11:06:24 -0300

    Adicionando ID a entidade do produto

--- lib/entities/product_entity.dart
+++ lib/entities/product_entity.dart
@@ -1,10 +1,12 @@
 class ProductEntity {
+  String id;
   String name;
   String description;
   num price;
   String image;
 
   ProductEntity({
+    required this.id,
     required this.name,
     required this.description,
     required this.price,
@@ -13,6 +15,7 @@ class ProductEntity {
 
   factory ProductEntity.fromMap(Map<String, dynamic> map) {
     return ProductEntity(
+      id: map['id'] ?? '',
       name: map['name'] ?? '',
       description: map['description'] ?? '',
       price: map['price'] ?? 0,
