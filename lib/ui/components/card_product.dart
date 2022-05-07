0f1f3c1aeb4982cda50c6ccfd8e73d2fbae8da16
Talis <talismarchioro@gmail.com>
2022-05-07 02:33:42 -0300

    card do produto

--- lib/ui/components/card_product.dart
+++ lib/ui/components/card_product.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uudi_challenge/entities/product_entity.dart';
+import 'package:uudi_challenge/ui/components/added_cart_dialog.dart';
+import 'package:uudi_challenge/ui/components/button_widget.dart';
 
 class CardProduct extends StatelessWidget {
   final ProductEntity product;
@@ -19,11 +21,14 @@ class CardProduct extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12)
               ),
-              child: Image.network(
-                product.image,
-                height: 100,
-                width: 100,
-                fit: BoxFit.cover,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Image.network(
+                  product.image,
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             Expanded(
@@ -32,26 +37,40 @@ class CardProduct extends StatelessWidget {
                   borderRadius: BorderRadius.circular(12)
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        product.name,
-                        style: TextStyle(
-                          color: Colors.grey[700],
-                          fontWeight: FontWeight.w400,
-                          fontSize: 22
-                        )
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontWeight: FontWeight.w400,
+                              fontSize: 22
+                            )
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF00bee0),
+                              fontSize: 18
+                            )
+                          )
+                        ],
                       ),
-                      Text(
-                        "R\$ ${product.price.toStringAsFixed(2)}",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF00bee0),
-                          fontSize: 18
-                        )
+                      ButtonWidget(
+                        label: "Comprar",
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (_) => const AddedCardDialog()
+                          );
+                        },
                       )
                     ],
                   ),
