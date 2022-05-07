8459de56e02b6489cd432bb9cfd2ccf5c3258aeb
Talis <talismarchioro@gmail.com>
2022-05-07 02:33:20 -0300

    implementacao do botao

--- lib/ui/components/button_widget.dart
+++ lib/ui/components/button_widget.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ButtonWidget extends StatelessWidget {
-  const ButtonWidget({ Key? key }) : super(key: key);
+  final String label;
+  final VoidCallback onPressed;
+  final bool outline;
+  const ButtonWidget({ Key? key, required this.label, required this.onPressed, this.outline = false}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(outline ? Colors.white :const Color(0xFF00bee0)),
+        shape: MaterialStateProperty.all<OutlinedBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+            side: const BorderSide(color: Color(0xFF00bee0))
+          )
+        ),
+      ),
+      child:  Text(
+        label,
+        style: TextStyle(
+          color: outline ? const Color(0xFF00bee0) : Colors.white
+        )
+      )
     );
   }
 }
